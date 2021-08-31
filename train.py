5e372d4078d673e183204ffb2845290040a01c69
heebin <1999bin@gmail.com>
2021-08-31 16:57:31 +0900

    cxxxx

--- train.py
+++ train.py
@@ -178,7 +178,7 @@ if __name__ == '__main__':
                                                                                                 opt.recalls.split(',')]
     save_name_pre = '{}_{}_{}'.format(data_name, crop_type, feature_dim)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     results = {'train_loss': [], 'train_accuracy': []}
     for recall_id in recalls:
         results['test_dense_recall@{}'.format(recall_id)] = []
@@ -196,12 +196,28 @@ if __name__ == '__main__':
         gallery_data_loader = DataLoader(gallery_data_set, batch_size, shuffle=False, num_workers=4*torch.cuda.device_count())
         eval_dict['gallery'] = {'data_loader': gallery_data_loader}
 
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print('Device:', device)
+    print('Current cuda device:', torch.cuda.current_device())
+    print('Count of using GPUs:', torch.cuda.device_count())
+
     # model setup, model profile, optimizer config and loss definition
-    model = ConfidenceControl(feature_dim, 2 * len(train_data_set.class_to_idx))
+
+
     if (device.type == 'cuda') and torch.cuda.device_count() > 1:
         print("multi GPU activate")
-        model = nn.DataParallel(model)
-    model.to(device)
+        model = ConfidenceControl(feature_dim, 2 * len(train_data_set.class_to_idx)).cuda()
+        model = nn.DataParallel(model).to(device)
+    elif (device.type == 'cuda') and torch.cuda.device_count() == 1:
+        print("single GPU activate")
+        model = ConfidenceControl(feature_dim, 2 * len(train_data_set.class_to_idx))
+        model = model.to(device)
+    else:
+        print("cpu mode")
+        model = ConfidenceControl(feature_dim, 2 * len(train_data_set.class_to_idx))
+
+
     flops, params = profile(model, inputs=(torch.randn(1, 3, 224, 224).to(device), True, None))
     flops, params = clever_format([flops, params])
     print('# Model Params: {} FLOPs: {}'.format(params, flops))
